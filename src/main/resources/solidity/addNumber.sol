ba72a7d2963915581e4572264217699bee5e393c
xiaoran1985 <ldl734@126.com>
2022-05-31 22:06:33 +0800

    solidity file

---
+++ src/main/resources/solidity/addNumber.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract AddNumber{
+    function sum(uint256 a,uint256 b) public pure returns(uint256){
+        return a + b;
+    }
+}
